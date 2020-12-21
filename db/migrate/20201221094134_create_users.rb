068094bc2b9124accce3819772c72cd008cb498f
Lavanya230759 <gellankalavanya2307@gmail.com>
2020-12-21 16:33:01 +0530

    first commit

---
+++ db/migrate/20201221094134_create_users.rb
@@ -0,0 +1,8 @@
+class CreateParticipants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/participant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Participant, type: :model do
+
+  describe 'attribute' do
+   it 'should have a name' do
+   end
+
+   it 'should have age' do
+   end
+
+   it ' should have gender' do
+   end
+ end
+end
